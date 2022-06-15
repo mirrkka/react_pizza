4d17f3aa8a0839f66d9d1743d90c8585d280d6ba
a.sokolova <a.sokolova@linksat.ru>
2022-06-15 16:12:57 +0300

    first commit

---
+++ src/components/Categories.jsx
@@ -0,0 +1,31 @@
+import {useState} from 'react'
+
+
+function Categories(){
+
+    const categories = [
+        'Все',
+        'Мясные',
+        'Вегентарианская',
+        'Гриль',
+        'Острые', 
+        'Закрытые'    
+    ]
+
+    const [activeIndex, setActiveIndex] = useState(0);
+
+    const onClickCategory = (index) => {
+        setActiveIndex(index)
+    }
+    return (
+      <div className="categories">
+                <ul>
+                  {categories.map((value, i) => (
+                      <li onClick = {() => {onClickCategory(i)}} className={activeIndex === i ? "active" : ''}>{value}</li>
+                  ))}
+                </ul>
+              </div>
+    )
+  }
+
+  export default Categories
